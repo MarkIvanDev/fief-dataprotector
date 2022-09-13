c6d320244afe8c69d93300028b3d97fd7b1baa59
Mark Ivan Basto <markivan.developer@outlook.com>
2022-09-13 15:53:43 +0800

    implement DataProtectorService

---
+++ Fief.DataProtector/Fief.DataProtector/DataProtectorService.netstandard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fief.DataProtector
+{
+    public partial class DataProtectorService
+    {
+        static Task<string> PlatformEncrypt(string data)
+            => throw new NotImplementedException();
+
+        static Task<string> PlatformDecrypt(string data)
+            => throw new NotImplementedException();
+
+        static Task<byte[]> PlatformEncrypt(byte[] data)
+            => throw new NotImplementedException();
+
+        static Task<byte[]> PlatformDecrypt(byte[] data)
+            => throw new NotImplementedException();
+    }
+}
---
+++ Fief.DataProtector/Fief.DataProtector/DataProtectorService.shared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Fief.DataProtector
+{
+    public partial class DataProtectorService
+    {
+        private readonly string protectionDescriptor;
+
+        public DataProtectorService(string protectionDescriptor = null)
+        {
+            this.protectionDescriptor = protectionDescriptor;
+        }
+
+        public Task<string> Encrypt(string data)
+            => PlatformEncrypt(data);
+
+        public Task<string> Decrypt(string data)
+            => PlatformDecrypt(data);
+
+        public Task<byte[]> Encrypt(byte[] data)
+            => PlatformEncrypt(data);
+
+        public Task<byte[]> Decrypt(byte[] data)
+            => PlatformDecrypt(data);
+    }
+}
